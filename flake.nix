183fc33f6c2b86c8ea3ad2668e53daf92677334e
Moraxyc <i@qaq.li>
2026-01-08 18:56:24 +0800

    chore: add nix

---
+++ flake.nix
@@ -0,0 +1,95 @@
+{
+  description = "mpv-danmaku development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    systems.url = "github:nix-systems/default";
+
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+
+    crane.url = "github:ipetkov/crane";
+
+    advisory-db = {
+      url = "github:rustsec/advisory-db";
+      flake = false;
+    };
+  };
+
+  outputs =
+    inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } (
+      { self, ... }:
+      {
+        systems = import inputs.systems;
+
+        perSystem =
+          {
+            pkgs,
+            self',
+            ...
+          }:
+          let
+            craneLib = inputs.crane.mkLib pkgs;
+
+            src = craneLib.cleanCargoSource ./.;
+
+            commonArgs = {
+              inherit src;
+              strictDeps = true;
+
+              buildInputs = with pkgs; [ openssl ] ++ lib.optionals stdenv.isDarwin [ libiconv ];
+
+              nativeBuildInputs = with pkgs; [ pkg-config ];
+
+              OPENSSL_NO_VENDOR = true;
+            };
+
+            cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+          in
+          {
+            packages = {
+              mpv-danmaku = craneLib.buildPackage (
+                commonArgs
+                // {
+                  inherit cargoArtifacts;
+                  doCheck = true;
+                }
+              );
+              default = self'.packages.mpv-danmaku;
+            };
+
+            checks = {
+              inherit (self'.packages) mpv-danmaku;
+
+              clippy = craneLib.cargoClippy (
+                commonArgs
+                // {
+                  inherit cargoArtifacts;
+                  cargoClippyExtraArgs = "--all-targets -- --deny warnings";
+                }
+              );
+
+              fmt = craneLib.cargoFmt {
+                inherit src;
+              };
+
+              audit = craneLib.cargoAudit {
+                inherit src;
+                advisory-db = inputs.advisory-db;
+              };
+            };
+
+            devShells.default = craneLib.devShell {
+              inherit (self') checks;
+
+              packages = with pkgs; [ rust-analyzer ];
+
+              env.OPENSSL_NO_VENDOR = true;
+            };
+          };
+      }
+    );
+}
